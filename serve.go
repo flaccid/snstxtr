8952e38f44125fc233b14335edc577ecb2818d3c
Chris Fordham <cfordham@scentregroup.com>
2018-05-03 20:10:55 +1000

    Add listen port const and improve some log feedback for serving.

--- serve.go
+++ serve.go
@@ -1,20 +1,26 @@
 package snstxtr
 
 import (
+	"fmt"
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
 )
 
+const (
+	listenPort = 8080
+)
+
 func Serve() {
-	log.Printf("start snstxtr")
+	log.Printf("initialize snstxtr")
 	log.Debug("debug logging enabled")
 
 	http.HandleFunc("/", reqHandler)
 	http.HandleFunc("/health", healthCheckHandler)
 	http.HandleFunc("/health/", healthCheckHandler)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Info("listening for requests on :" + fmt.Sprintf("%v", listenPort))
+	if err := http.ListenAndServe(":"+fmt.Sprintf("%v", listenPort), nil); err != nil {
 		log.Fatalf("http; %v", err)
 	}
 }
